ad1494950322a95c1d33925124e62142f3207ef7
lw <lw@example.com>
2026-05-28 06:49:40 +0000

    Update backup configs and remove old start5GC directory
    - Update backup_/config files with latest configuration changes
    - Move some configs from start5GC to backup_ (smfcfg.yaml.bak, uecfg configs)
    - Remove entire start5GC directory (replaced by 5GC deployment package)

--- backup_/config/upf-iptables.sh
+++ backup_/config/upf-iptables.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-#
-# Configure iptables in UPF
-#
-iptables -t nat -A POSTROUTING -o eth0  -j MASQUERADE
+#!/bin/sh
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -I FORWARD 1 -j ACCEPT
-
